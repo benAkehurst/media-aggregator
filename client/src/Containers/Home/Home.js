edc121b9cfd1cfb8c87ec8f9ee85bcadd011aa3b
benakehurst <benakehurst@hotmail.com>
2020-05-04 12:45:11 +0100

    feat(client): basic scafollding for homepage

--- client/src/Containers/Home/Home.js
+++ client/src/Containers/Home/Home.js
@@ -8,7 +8,19 @@ class Home extends Component {
   render() {
     return (
       <Aux>
-        <div>Home</div>
+        <div className={classes.HeaderWrapper}>Header</div>
+        <div className={classes.SelectorWrapper}>
+          <div className={classes.DateTimeWrapper}>
+            <div className={classes.CalenderWrapper}>Calendar</div>
+            <div className={classes.TimesWrapper}>Times</div>
+          </div>
+          <div className={classes.SiteOptionsWrapper}>Options</div>
+        </div>
+        <div className={classes.ViewerWrapper}>
+          <div className={classes.NewsImageWrapper}>Image</div>
+          <div className={classes.NewsHeadlineWrapper}>Headline</div>
+          <div className={classes.SharingWrapper}>Sharing</div>
+        </div>
       </Aux>
     );
   }
