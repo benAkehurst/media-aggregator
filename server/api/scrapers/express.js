0c5ca640ca6a793a4cfbc2c96b1eb269ddafeebe
benakehurst <benakehurst@hotmail.com>
2020-05-04 09:06:20 +0100

    feat(server): add script to get express

--- server/api/scrapers/express.js
+++ server/api/scrapers/express.js
@@ -1,7 +1,105 @@
 const puppeteer = require('puppeteer');
+const cloudinary = require('cloudinary').v2;
+const moment = require('moment');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 const express = async (url) => {
-  return url;
+  const d = new Date();
+  const date = moment(new Date()).format('DD/MM/YYYY');
+
+  // Set file name for cloudinary
+  const cloudinary_options = {
+    public_id: `newsshot/${url.name}_${d}`,
+  };
+
+  // Define puppeteer instance
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+    ],
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+  });
+
+  // Launch scraper
+  const page = await browser.newPage();
+  await page.goto(url.url);
+  await page.waitFor(2000);
+
+  // hide cookie popup
+  await page.evaluate(() => {
+    let cookies = document.querySelector('#qcCmpUi');
+    cookies.style.visibility = 'hidden';
+  });
+
+  // change page bg color
+  await page.evaluate(() => {
+    let bg = document.querySelector('.qc-cmp-showing');
+    bg.style.opacity = 0;
+  });
+
+  await page.waitFor(1000);
+
+  // hide top ad
+  await page.evaluate(() => {
+    let cookies = document.querySelector('.superbanner');
+    cookies.style.display = 'none';
+  });
+  await page.waitFor(1000);
+
+  // Take Screenshot
+  let shotResult = await page
+    .screenshot()
+    .then((result) => {
+      console.log(`${url.name} got some results.`);
+      return result;
+    })
+    .catch((e) => {
+      console.error(`[${url.name}] Error in snapshotting news`, e);
+      return false;
+    });
+
+  // Upload screenshot to Cloudinary
+  let generateScreenshotAndNewsObj = cloundinaryPromise(
+    shotResult,
+    cloudinary_options
+  ).then((res) => {
+    return (newsObject = {
+      url: url.url,
+      name: url.name,
+      date: date,
+      headline: headline,
+      screenshotUrl: res.secure_url,
+    });
+  });
+
+  // Close browser
+  browser.close();
+  return generateScreenshotAndNewsObj;
 };
+
+function cloundinaryPromise(shotResult, cloudinary_options) {
+  return new Promise(function (res, rej) {
+    cloudinary.uploader
+      .upload_stream(cloudinary_options, function (error, cloudinary_result) {
+        if (error) {
+          console.error('Upload to cloudinary failed: ', error);
+          rej(error);
+        }
+        res(cloudinary_result);
+      })
+      .end(shotResult);
+  });
+}
 
 module.exports = express;
