76a728101ef3265fff69725c0f534895203bcca6
benakehurst <benakehurst@hotmail.com>
2020-05-05 10:59:09 +0100

    feat(client): add options for news items

---
+++ client/src/Helpers/newsOptions.js
@@ -0,0 +1,9 @@
+export function getNewsItems() {
+  return [
+    { news: 'bbc', initials: 'BBC', bgColor: '#bb1919', color: 'white' },
+    { news: 'dailymail', initials: 'DM', bgColor: '#004cb3', color: 'white' },
+    { news: 'guardian', initials: 'G', bgColor: '#052a62', color: 'white' },
+    { news: 'express', initials: 'EXP', bgColor: '#e61b40', color: 'white' },
+    { news: 'telegraph', initials: 'TELE', bgColor: '#2e9f9a', color: 'white' },
+  ];
+}
