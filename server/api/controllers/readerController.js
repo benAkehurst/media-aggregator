0d5ef2fdebb78673df8ec6833a967a44582af4f6
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2022-11-10 17:02:39 +0000

    refactor reader routes
    - change all naming methods to camalCase
    - change all methods to async
    - add proper valiadtion to methods
    - add correct http error codes
    - update response objects

--- server/api/controllers/readerController.js
+++ server/api/controllers/readerController.js
@@ -1,100 +1,149 @@
-'use strict';
 const mongoose = require('mongoose');
 const NewsClipping = mongoose.model('NewsClipping');
 
 const { formatTime } = require('./../../helpers/timeAndDate');
 
-exports.list_all_clippings_from_single_day = (req, res) => {
+exports.listAllClippingsFromSingleDay = async (req, res) => {
   /**
-   * TODO:
-   * query structure: client needs to send DD/MM/YYYY
-   * {
-        "date":"02/05/2020"
-      }
+   * query structure: ?date=08/11/2022
    */
-  const date = req.body.date;
-  NewsClipping.find({ date: date }, (err, clippings) => {
-    if (err) {
-      return res.status(500).json({
-        error: err,
-        message: 'No clippings fround',
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      message: 'All clippings',
-      data: clippings,
+  const { date } = req.query;
+  if (!date) {
+    return res.status(400).json({
+      success: false,
+      message: `no date provided`,
     });
-  });
+  }
+  try {
+    NewsClipping.find({ date: date }, (err, clippings) => {
+      if (err) {
+        return res.status(500).json({
+          error: err,
+          message: 'No clippings fround',
+        });
+      }
+      return res.status(200).json({
+        message: 'All clippings',
+        data: clippings,
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: err,
+      message: 'error getting all clippings from date',
+    });
+  }
 };
 
-exports.list_all_clippings_from_hour = (req, res) => {
+exports.listAllClippingsFromHour = async (req, res) => {
   /**
-   * TODO:
-   * query structure: client needs to send  new Date().getHours() <- this will give a number between 1 & 23
-   * {
-        "date":"02/05/2020",
-        "lowTime":"10",
-        "hightTime":"11"
-      }
+   * query structure: ?date=10/11/2022&lowTime=13&highTime=14
    */
-  const date = req.body.date;
-  const lowTime = req.body.lowTime;
-  const highTime = req.body.highTime;
+  const { date, lowTime, highTime } = req.query;
   const formattedTime = formatTime(date, lowTime, highTime);
-  NewsClipping.find({ date: date }, (err, clippings) => {
-    if (err) {
-      return res.status(500).json({
-        error: err,
-        message: 'No clippings fround',
+  if (!date || !lowTime || !highTime) {
+    return res.status(400).json({
+      success: false,
+      message: `no date or times provided`,
+    });
+  }
+  try {
+    NewsClipping.find({ date: date }, (err, clippings) => {
+      if (err) {
+        return res.status(500).json({
+          error: err,
+          message: 'No clippings fround',
+        });
+      }
+      let filteredClippings = clippings.filter((clipping) => {
+        const time = new Date(clipping.created_at).getTime();
+        return formattedTime.lowTime < time && formattedTime.highTime < time
+          ? clipping
+          : null;
       });
-    }
-    let filteredClippings = clippings.filter((clipping) => {
-      const time = new Date(clipping.created_at).getTime();
-      console.log(time);
-      return formattedTime.lowTime < time && formattedTime.highTime < time
-        ? clipping
-        : null;
+      if (filteredClippings.length < 1) {
+        return res.status(200).json({
+          success: true,
+          message: 'No clippings found for this time',
+          data: filteredClippings,
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: 'Clippings found',
+        data: filteredClippings,
+      });
     });
-    return res.status(200).json({
-      success: true,
-      message: 'Clippings found',
-      data: filteredClippings,
+  } catch (error) {
+    return res.status(500).json({
+      error: err,
+      message: 'error getting all clippings from the last hour',
     });
-  });
+  }
 };
 
-exports.list_all_clippings_from_news_source = (req, res) => {
-  const date = req.body.data.date;
-  const newsSite = req.body.data.newsSite;
-  NewsClipping.find({ date: date, name: newsSite }, (err, clippings) => {
-    if (err) {
-      return res.status(500).json({
-        error: err,
-        message: 'No clippings fround',
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      message: 'All clippings',
-      data: clippings,
+exports.listAllClippingsFromNewsSource = async (req, res) => {
+  /**
+   * query structure: ?newsSite=guardian
+   */
+  const { newsSite } = req.query;
+  if (!newsSite) {
+    return res.status(400).json({
+      success: false,
+      message: `no newsSite provided`,
     });
-  });
+  }
+  try {
+    NewsClipping.find({ name: newsSite }, (err, clippings) => {
+      if (err) {
+        return res.status(500).json({
+          error: err,
+          message: 'No clippings fround',
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: 'All clippings',
+        data: clippings,
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: err,
+      message: `error getting all clippings from ${newsSite}`,
+    });
+  }
 };
 
-exports.read_single_clipping = (req, res) => {
-  NewsClipping.findById(req.params.clippingId, (err, clipping) => {
-    if (err) {
-      res.status(400).json({
-        status: false,
-        message: "Couldn't find clipping",
-        code: 400,
-      });
-    }
-    res.status(200).json({
-      status: true,
-      message: 'Clipping found',
-      data: clipping,
+exports.readSingleClipping = async (req, res) => {
+  /**
+   * query structure: ?clippingId=636aa3e502bdf34750b261a6
+   */
+  const { clippingId } = req.query;
+  if (!clippingId) {
+    return res.status(400).json({
+      success: false,
+      message: `no id provided`,
     });
-  });
+  }
+  try {
+    NewsClipping.findById(req.query.clippingId, (err, clipping) => {
+      if (err) {
+        res.status(500).json({
+          status: false,
+          message: "Couldn't find clipping",
+        });
+      }
+      res.status(200).json({
+        status: true,
+        message: 'Clipping found',
+        data: clipping,
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: err,
+      message: `error getting all clippings from ${newsSite}`,
+    });
+  }
 };
