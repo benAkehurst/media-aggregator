a043840b887474d958dc550672ee258121f2970b
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2022-11-10 16:36:56 +0000

    change route methods to get

--- server/api/routes/readerRoutes.js
+++ server/api/routes/readerRoutes.js
@@ -4,16 +4,16 @@ module.exports = (app) => {
 
   // Reader Routes
   app
-    .route('/api/reader/single-day')
-    .post(reader.list_all_clippings_from_single_day);
+    .route('/api/reader/single-day/')
+    .get(reader.list_all_clippings_from_single_day);
 
   app
     .route('/api/reader/single-hour/')
-    .post(reader.list_all_clippings_from_hour);
+    .get(reader.list_all_clippings_from_hour);
 
   app
     .route('/api/reader/single-news-source/')
     .post(reader.list_all_clippings_from_news_source);
 
-  app.route('/api/reader/:clippingId').get(reader.read_single_clipping);
+  app.route('/api/reader/').get(reader.read_single_clipping);
 };
