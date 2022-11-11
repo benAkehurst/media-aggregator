ced3649e6545b38d081f6af7025e1cfd6d832360
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2022-11-11 15:47:14 +0000

    update date and time methods

--- server/helpers/timeAndDate.js
+++ server/helpers/timeAndDate.js
@@ -1,5 +1,13 @@
 exports.formatTime = async (date, lowTime, highTime) => {
-  const parsedDate = await swapArrayElements(date, 1, 4).join('');
+  const swapArrayElements = (arr, a, b) => {
+    let _arr = [...arr];
+    let temp = _arr[a];
+    _arr[a] = _arr[b];
+    _arr[b] = temp;
+    return _arr;
+  };
+
+  const parsedDate = swapArrayElements(date, 1, 4).join('');
   const parsedLowTime = parseInt(lowTime);
   const parsedHighTime = parseInt(highTime);
   let dateObject = new Date(parsedDate);
@@ -8,12 +16,4 @@ exports.formatTime = async (date, lowTime, highTime) => {
     .setHours(parsedHighTime, 0, 0, 0)
     .toString();
   return { lowTime: lowTimeFormatted, highTime: highTimeFormatted };
-};
-
-const swapArrayElements = async (arr, a, b) => {
-  let _arr = [...arr];
-  let temp = _arr[a];
-  _arr[a] = _arr[b];
-  _arr[b] = temp;
-  return _arr;
 };
