913c382b6dfd44bd3bba4f37ba42e1d39a24e90a
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2022-11-11 12:24:49 +0000

    add route to get news sources from BE

--- server/api/routes/readerRoutes.js
+++ server/api/routes/readerRoutes.js
@@ -3,15 +3,13 @@ module.exports = (app) => {
   const reader = require('../controllers/readerController');
 
   // Reader Routes
+  app.route('/api/reader/').get(reader.readSingleClipping);
+  app.route('/api/reader/news-sources').get(reader.getAllSources);
+  app.route('/api/reader/single-hour/').get(reader.listAllClippingsFromHour);
   app
     .route('/api/reader/single-day/')
     .get(reader.listAllClippingsFromSingleDay);
-
-  app.route('/api/reader/single-hour/').get(reader.listAllClippingsFromHour);
-
   app
     .route('/api/reader/single-news-source/')
     .get(reader.listAllClippingsFromNewsSource);
-
-  app.route('/api/reader/').get(reader.readSingleClipping);
 };
