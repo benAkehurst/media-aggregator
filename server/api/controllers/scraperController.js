33eac1351b668166ee6f497bdb18250d55c88f94
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2022-11-11 16:27:48 +0000

    uncomment all methods

--- server/api/controllers/scraperController.js
+++ server/api/controllers/scraperController.js
@@ -13,30 +13,30 @@ const newsUrls = NEWS_SOURCES;
 
 const scrapeV1 = async (req, res) => {
   try {
-    // await bbc(newsUrls[0])
-    //   .then((res) => {
-    //     createNewClipping(res);
-    //     console.log('bbc finished');
-    //   })
-    //   .catch((err) => console.log(err));
-    // await dm(newsUrls[1])
-    //   .then((res) => {
-    //     createNewClipping(res);
-    //     console.log('daily mail finished');
-    //   })
-    //   .catch((err) => console.log(err));
-    // await guardian(newsUrls[2])
-    //   .then((res) => {
-    //     createNewClipping(res);
-    //     console.log('gaurdian finished');
-    //   })
-    //   .catch((err) => console.log(err));
-    // await express(newsUrls[3])
-    //   .then((res) => {
-    //     createNewClipping(res);
-    //     console.log('express finished');
-    //   })
-    //   .catch((err) => console.log(err));
+    await bbc(newsUrls[0])
+      .then((res) => {
+        createNewClipping(res);
+        console.log('bbc finished');
+      })
+      .catch((err) => console.log(err));
+    await dm(newsUrls[1])
+      .then((res) => {
+        createNewClipping(res);
+        console.log('daily mail finished');
+      })
+      .catch((err) => console.log(err));
+    await guardian(newsUrls[2])
+      .then((res) => {
+        createNewClipping(res);
+        console.log('gaurdian finished');
+      })
+      .catch((err) => console.log(err));
+    await express(newsUrls[3])
+      .then((res) => {
+        createNewClipping(res);
+        console.log('express finished');
+      })
+      .catch((err) => console.log(err));
     await telegraph(newsUrls[4])
       .then((res) => {
         createNewClipping(res);
